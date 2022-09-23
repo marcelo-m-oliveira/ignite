bdd1f5b59338557e0bd87cd05bfd4a2ec1f670c6
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-23 13:29:31 -0300

    Adicionando component de Post;

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import { Sidebar } from './components/sidebar/Sidebar'
 import { Header } from './components/header/Header'
+import { Post } from './components/post/Post'
 
 import styles from './App.module.css'
 import './global.css'
+
 
 export function App() {
   return (
@@ -11,6 +13,8 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
+          <Post />
+          <Post />
         </main>
       </div>
     </div>
--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,7 +1,31 @@
-import styles from '.Post.module.css'
+import styles from './Post.module.css'
 
-export function Post(props) {
+export function Post() {
   return (
-    <h1>batata</h1>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src='https://github.com/marcelo-m-oliveira.png' />
+          <div className={styles.authorInfo}>
+            <strong>Marcelo M Oliveira</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+        <time title='23 de Setembro às 08:56h' dateTime='2022-09-23 08:56:32'>Publicado há 4h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>
+          👉{' '}<a href=''>jane.design/doctorcare</a>
+        </p>
+        <p>
+          <a href=''>#novoprojeto</a>{' '}
+          <a href=''>#nlw</a>{' '}
+          <a href=''>#rocketseat</a>
+        </p>
+      </div>
+    </article>
   )
 }
