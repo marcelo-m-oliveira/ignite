52525115f29ac88eca7b833ac7d5a5f9a8fb2b57
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-23 12:34:14 -0300

    Criando header; Criando carde de profile;

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import './styles.css'
-import { Header } from './components/Header'
+import { Sidebar } from './components/sidebar/Sidebar'
+import { Header } from './components/header/Header'
+
+import styles from './App.module.css'
+import './global.css'
+
 export function App() {
   return (
     <div>
       <Header />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        </main>
+      </div>
     </div>
 
   )
---
+++ src/components/post/Post.jsx
@@ -0,0 +1,7 @@
+import styles from '.Post.module.css'
+
+export function Post(props) {
+  return (
+    <h1>batata</h1>
+  )
+}
