d14d3172eaef3bf3a6e47d79de88f86e07bfe7c5
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2022-09-28 02:41:09 -0300

    Adicionando funcionalidade de link;

--- src/components/commennt/Comment.jsx
+++ src/components/commennt/Comment.jsx
@@ -1,12 +1,23 @@
+import { useState } from 'react'
+
 import { ThumbsUp, Trash } from 'phosphor-react'
 import { Avatar } from './../avatar/Avatar'
 
 import styles from './Comment.module.css'
 
 export function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0)
+
   function handleDeleteComment() {
     onDeleteComment(content)
   }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    })
+  }
+
   return (
     <div className={styles.comment}>
       <Avatar hasBorder={false} src='https://scontent.fssa14-1.fna.fbcdn.net/v/t39.30808-6/307885363_3334231036862939_1770154639640304178_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=agQ_xBlzuFEAX_ixvTm&_nc_ht=scontent.fssa14-1.fna&oh=00_AT-BXKXEyYlQWk0iUqm6ubUnEh6uuMjbuOOkvfqeYWrHZw&oe=63335798' />
@@ -24,9 +35,9 @@ export function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
