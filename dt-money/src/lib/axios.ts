47522086000753c0a5dae1c153a0445d6f213463
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2023-02-25 15:36:11 -0300

    Relação de modificações:
    — Ajustando um service fake para API

--- dt-money/src/lib/axios.ts
+++ dt-money/src/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL:
+    'https://my-json-server.typicode.com/marcelo-m-oliveira/ignite/tree/main/dt-money',
 })
