5d5f8d5b7ff73a45f2c8ff55033ce03977004006
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2023-02-26 01:43:42 -0300

    Relação de modificações: — Criando o component home da aplicação — Configurando API externa para aplicação — Populando tela home com dados da API

--- shop/src/pages/index.tsx
+++ shop/src/pages/index.tsx
@@ -1,3 +1,66 @@
-export default function Home() {
-  return <h1>Hello world</h1>
+import { HomeContainer, Product } from '@/src/styles/pages/home'
+import { GetServerSideProps } from 'next'
+import Image from 'next/image'
+
+import { useKeenSlider } from 'keen-slider/react'
+import 'keen-slider/keen-slider.min.css'
+
+import { stripe } from '../lib/stripe'
+import Stripe from 'stripe'
+import { priceFormatter } from '../utils/formatter'
+
+interface HomeProps {
+  products: {
+    id: string
+    name: string
+    imageUrl: string
+    price: number
+  }[]
+}
+
+export default function Home({ products }: HomeProps) {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    },
+  })
+  return (
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      {products.map((product) => {
+        return (
+          <Product key={product.id} className="keen-slider__slide">
+            <Image src={product.imageUrl} width={520} height={480} alt="" />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{priceFormatter.format(product.price)}</span>
+            </footer>
+          </Product>
+        )
+      })}
+    </HomeContainer>
+  )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list({
+    expand: ['data.default_price'],
+  })
+
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price
+
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: price.unit_amount! / 100,
+    }
+  })
+
+  return {
+    props: {
+      products,
+    },
+  }
 }
---
+++ shop/src/styles/index.ts
@@ -0,0 +1,32 @@
+import { createStitches } from '@stitches/react'
+
+export const {
+  config,
+  styled,
+  globalCss,
+  keyframes,
+  getCssText,
+  theme,
+  createTheme,
+  css,
+} = createStitches({
+  theme: {
+    colors: {
+      white: '#fff',
+      gray900: '#121214',
+      gray800: '#202024',
+      gray300: '#c4c4cc',
+      gray100: '#e1e1e6',
+
+      purple500: '#a855f7',
+      purple300: '#d8b4fe',
+    },
+
+    fontSizes: {
+      md: '1.125rem',
+      lg: '1.25rem',
+      xl: '1.5rem',
+      '2xl': '2rem',
+    },
+  },
+})
