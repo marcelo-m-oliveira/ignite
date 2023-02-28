c44e4fc3eb9b6e72e32eef43f6fc5671ca13b0e0
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2023-02-28 18:44:53 -0300

    Relação de modificações:
    — Configurando o prisma adapter para manipular os dados enviados no banco de dados da aplicação

--- ignate-call/src/pages/api/auth/[...nextauth].api.ts
+++ ignate-call/src/pages/api/auth/[...nextauth].api.ts
@@ -1,31 +1,44 @@
-import NextAuth, { NextAuthOptions } from 'next-auth'
+import type { NextApiRequest, NextApiResponse } from 'next'
+
 import GoogleProvider from 'next-auth/providers/google'
+import NextAuth, { NextAuthOptions } from 'next-auth'
 
-export const authOptions: NextAuthOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID ?? '',
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
-      authorization: {
-        params: {
-          scope:
-            'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+import { PrismaAdapter } from './../../../lib/auth/prisma-adapter'
+
+export function buildNextAuthOptions(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): NextAuthOptions {
+  return {
+    adapter: PrismaAdapter(req, res),
+
+    providers: [
+      GoogleProvider({
+        clientId: process.env.GOOGLE_CLIENT_ID ?? '',
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? '',
+        authorization: {
+          params: {
+            scope:
+              'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/calendar',
+          },
         },
-      },
-    }),
-  ],
+      }),
+    ],
 
-  callbacks: {
-    async signIn({ account }) {
-      if (
-        !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
-      ) {
-        return '/register/connect-calendar/?error=permissions'
-      } else {
-        return true
-      }
+    callbacks: {
+      async signIn({ account }) {
+        if (
+          !account?.scope?.includes('https://www.googleapis.com/auth/calendar')
+        ) {
+          return '/register/connect-calendar/?error=permissions'
+        } else {
+          return true
+        }
+      },
     },
-  },
+  }
 }
 
-export default NextAuth(authOptions)
+export default async function auth(req: NextApiRequest, res: NextApiResponse) {
+  return await NextAuth(req, res, buildNextAuthOptions(req, res))
+}
