4a310d0637255af2c2ab9eac2d8821ea1301451d
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2023-03-01 13:51:10 -0300

    Relação de modificações:
    — Criando um atalho para levantar o primsa studio
    — Criando a tela de calendario para registro dos dias
    — Atualizando o util formater

---
+++ ignite-call/src/pages/register/time-intervals/index.page.tsx
@@ -0,0 +1,124 @@
+import {
+  Button,
+  Checkbox,
+  Heading,
+  MultiStep,
+  Text,
+  TextInput,
+} from '@ignite-ui/react'
+
+import { ArrowRight } from 'phosphor-react'
+
+import { useForm, useFieldArray, Controller } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Container, Header } from '../styles'
+
+import { getWeekDays } from '@/utils/formatter'
+
+import {
+  IntervalBox,
+  IntervalDay,
+  IntervalInputs,
+  IntervalItem,
+  IntervalsContainer,
+} from './styles'
+
+const timeIntervalsFormSchema = z.object({})
+
+export default function TimeIntervals() {
+  const {
+    register,
+    handleSubmit,
+    control,
+    watch,
+    formState: { isSubmitting, errors },
+  } = useForm({
+    defaultValues: {
+      intervals: [
+        { weekDay: 0, enabled: false, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 1, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 2, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 3, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 4, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 5, enabled: true, startTime: '08:00', endTime: '18:00' },
+        { weekDay: 6, enabled: false, startTime: '08:00', endTime: '18:00' },
+      ],
+    },
+  })
+
+  const weekDays = getWeekDays()
+
+  const { fields } = useFieldArray({
+    control,
+    name: 'intervals',
+  })
+
+  const intervals = watch('intervals')
+
+  async function handleSetTimeIntervals(data: any) {
+    console.log(data)
+  }
+
+  return (
+    <Container>
+      <Header>
+        <Heading as="strong">Quase lá</Heading>
+        <Text>
+          Defina o intervalo de horários que você está disponível em cada dia da
+          semana.
+        </Text>
+
+        <MultiStep size={4} currentStep={3} />
+      </Header>
+
+      <IntervalBox as="form" onSubmit={handleSubmit(handleSetTimeIntervals)}>
+        <IntervalsContainer>
+          {fields.map((field, index) => {
+            return (
+              <IntervalItem key={field.id}>
+                <IntervalDay>
+                  <Controller
+                    name={`intervals.${index}.enabled`}
+                    control={control}
+                    render={({ field }) => {
+                      return (
+                        <Checkbox
+                          onCheckedChange={(checked) => {
+                            field.onChange(checked === true)
+                          }}
+                          checked={field.value}
+                        />
+                      )
+                    }}
+                  />
+                  <Text>{weekDays[field.weekDay]}</Text>
+                </IntervalDay>
+                <IntervalInputs>
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.startTime`)}
+                  />
+                  <TextInput
+                    size="sm"
+                    type="time"
+                    step={60}
+                    disabled={intervals[index].enabled === false}
+                    {...register(`intervals.${index}.endTime`)}
+                  />
+                </IntervalInputs>
+              </IntervalItem>
+            )
+          })}
+        </IntervalsContainer>
+
+        <Button type="submit">
+          Próximo passo <ArrowRight />
+        </Button>
+      </IntervalBox>
+    </Container>
+  )
+}
--- ignite-call/src/utils/formatter.ts
+++ ignite-call/src/utils/formatter.ts
@@ -4,3 +4,13 @@ export const priceFormatter = new Intl.NumberFormat('pt-BR', {
   style: 'currency',
   currency: 'BRL',
 })
+
+export function getWeekDays() {
+  const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
+
+  return Array.from(Array(7).keys())
+    .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
+    .map((weekDay) => {
+      return weekDay.substring(0, 1).toUpperCase().concat(weekDay.substring(1))
+    })
+}
