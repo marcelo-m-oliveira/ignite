734adaf5cfcedd6f5d090bb655672f7b18717037
Marcelo M Oliveira <marcelomra.oliveira@gmail.com>
2023-03-02 12:41:50 -0300

    Relação de modificações:
    — Criando a tela de schedule
    — Criando um component global de calendario
    — Atualizando o css do styles global
    — Atualizando o formatter para pegar o name do dia da semana

---
+++ ignite-call/src/pages/schedule/[username]/ScheduleForm/CalendarStep/index.tsx
@@ -0,0 +1,42 @@
+import {
+  Container,
+  TimePicker,
+  TimePickerHeader,
+  TimePickerItem,
+  TimePickerList,
+} from '@/pages/schedule/[username]/ScheduleForm/CalendarStep/styles'
+import { Calendar } from '@/Calendar'
+
+export function CalendarStep() {
+  const isDateSelected = true
+  return (
+    <Container isTimePickerOpen={isDateSelected}>
+      <Calendar />
+
+      {isDateSelected && (
+        <TimePicker>
+          <TimePickerHeader>
+            Quarta-feira <span>20 de batata</span>
+          </TimePickerHeader>
+
+          <TimePickerList>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem disabled>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem disabled>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+            <TimePickerItem>08:00h</TimePickerItem>
+          </TimePickerList>
+        </TimePicker>
+      )}
+    </Container>
+  )
+}
--- ignite-call/src/styles/global.ts
+++ ignite-call/src/styles/global.ts
@@ -5,16 +5,22 @@ export const globalStyles = globalCss({
     boxSizing: 'border-box',
     padding: 0,
     margin: 0,
+
+    button: {
+      '&:not(:disabled):hover': {
+        transition: 'background 0.2s',
+      },
+    },
+
+    [`${Button}`]: {
+      '&:not(:disabled):hover': {
+        transition: 'background 0.2s',
+      },
+    },
   },
   body: {
     backgroundColor: '$gray900',
     color: '$gray100',
     '-webkit-font-smoothing': 'antialiased',
-
-    [`${Button}`]: {
-      '&:hover': {
-        transition: 'background 0.2s',
-      },
-    },
   },
 })
